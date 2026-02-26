7a04914e69744831af5af20cd6cc8a193f16006b
unknown <chmcs2026.prashik.kamble@gmail.com>
2026-02-26 08:16:33 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+//console.log("Prashik")
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
+import bookRoutes from "./routes/bookRoutes.js"
+import { connectDB } from "./config/db.js";
+import cors from "cors";
+
+const app=express(); 
+const port=process.env.PORT || 3001
+app.use(cors(
+    {
+        origin: 'http://localhost:5173'
+    }
+))
+// app.get('/',(req,res)=>{
+//     res.status(200).json("Hello")
+//     console.log('Hello')
+// })
+// app.listen(port,() =>{
+//     console.log(`http://localhost:${port}`)
+// })
+
+app.use(express.json())
+app.use("/books", bookRoutes)
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/books`)
+    })
+})
